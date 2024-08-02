4935d624662cecdd4aded106e927a50327e100e0
DongHyunTedKim <eastar29@naver.com>
2024-08-02 11:23:42 +0900

    Initial Commit

---
+++ HanYoungHater/HanYoungHaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanYoungHaterApp.swift
+//  HanYoungHater
+//
+//  Created by Ted Kim on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HanYoungHaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
